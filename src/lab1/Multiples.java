d90f67020858d22ca7c38b5e4fd1f6da9a2b7f81
Zachary Klouchnikov <zachary.klou@gmail.com>
2025-09-08 11:15:04 -0400

    Created Multiples.java

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,21 +3,32 @@ package lab1;
 public class Multiples {
 
     public static void main(String[] args) {
+        System.out.println(multiples(10, 3, 5));
+    }
+
+    static int multiples(int n, int a, int b) {
         int i = 1;
         int j = 0;
-        while (i < 1000) {
+        while (i < n) {
             i++;
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
 
-            if (divisibleBy3) {
+            if (divisibleBy3 && divisibleBy5) {
                 j++;
             }
-            if (divisibleBy5) {
-                j++;
+            else {
+                if (divisibleBy3) {
+                    j++;
+                }
+                if (divisibleBy5) {
+                    j++;
+                }
             }
+
         }
-        System.out.println(j);
+
+    return j;
     }
 }
